8fce9874afd36eb043b21c952f051418175d20fc
Cristobal <quilimacox1@gmail.com>
2024-08-11 13:20:29 -0500

    Se ajusta paginacion

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,20 +1,34 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getMoviesThunks } from "../store/slices/movies.slices";
 import "./styles/movieCard.css";
-import { Link } from "react-router-dom"; // Importa Link desde React Router
+import { Link } from "react-router-dom";
 
 const MovieCard = () => {
-  const [filter, setFilter] = useState("popular"); 
+  const [filter, setFilter] = useState("popular");
+  const [currentPage, setCurrentPage] = useState(1); // Estado para la página actual
+  const moviesPerPage = 5; // Número de películas por página
   const dispatch = useDispatch();
   const moviesData = useSelector(state => state.moviesGlobal);
 
   useEffect(() => {
-    dispatch(getMoviesThunks(filter)); 
+    dispatch(getMoviesThunks(filter));
   }, [dispatch, filter]);
+
+  // Calcular los índices de las películas que se mostrarán en la página actual
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = moviesData.slice(indexOfFirstMovie, indexOfLastMovie);
+
+  // Función para cambiar de página
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // Total de páginas
+  const totalPages = Math.ceil(moviesData.length / moviesPerPage);
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
+    setCurrentPage(1); // Reinicia la página a la primera cuando se cambia el filtro
   };
 
   return (
@@ -26,8 +40,8 @@ const MovieCard = () => {
         <button className="button__filter" onClick={() => handleFilterChange("movie")}>Top rated</button>
       </div>
       <section className="ssss">
-        {moviesData && moviesData.length > 0 ? (
-          moviesData.map(movie => (
+        {currentMovies && currentMovies.length > 0 ? (
+          currentMovies.map(movie => (
             <Link to={`/movie/${movie.id}`} className="movie__card" key={movie.id}>
               <img className="img_movie" src={movie.poster_path} alt="" />
               <h2>{movie.title}</h2>
@@ -38,6 +52,17 @@ const MovieCard = () => {
           <p>No hay datos de películas disponibles</p>
         )}
       </section>
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={`page-link ${index + 1 === currentPage ? 'active' : ''}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
