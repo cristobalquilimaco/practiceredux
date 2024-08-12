e2fe8311559dc118a4970ba8f04d35bb068024b0
Cristobal <quilimacox1@gmail.com>
2024-08-12 10:50:14 -0500

    Se ajusta menu brand

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">Navbar scroll</Link>
+        <Link className="navbar-brand" to="/">MovieHub</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
